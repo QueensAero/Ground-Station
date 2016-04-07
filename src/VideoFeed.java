5be70a63b7935aedb1165319070eb72a1a84387a
RyanJDick <rjdick95@yahoo.ca>
2016-04-07 18:49:45 -0400

    Added AffineTransform import
    I forgot to add this in my last commit

--- src/VideoFeed.java
+++ src/VideoFeed.java
@@ -9,6 +9,7 @@ import java.awt.Image;
 import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.GeneralPath;
 import java.awt.image.BufferedImage;
