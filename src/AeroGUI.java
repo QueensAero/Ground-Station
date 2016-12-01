d26de95b5e5594f52aab391e739f7f37dde48d2d
Ryan Dick <rjdick95@yahoo.ca>
2016-11-30 20:32:06 -0500

    Added a java.util.Logger to the project.

--- src/AeroGUI.java
+++ src/AeroGUI.java
@@ -1,10 +1,19 @@
 import java.awt.EventQueue;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.*;
+
+
 import javax.swing.JFrame;
 import java.awt.BorderLayout;
 import javax.swing.UIManager;
 
 public class AeroGUI {
-	//member variables
+	// Logger to be used by all classes in the project. (This line should be at the top of every class)
+	private static final Logger LOGGER = Logger.getLogger(AeroGUI.class.getName());
+	private static ConsoleHandler ch;
+	private static FileHandler fh;
 	private static MainWindow main;
 	private JFrame frame;
 	
@@ -31,6 +40,26 @@ public class AeroGUI {
 
 	/**	 Initialize the contents of the frame. */
 	private void initialize() {
+		// Set up logging  
+	    try {  
+	        // The possible log levels are: FINEST, FINER, FINE, CONFIG, INFO, WARNING, SEVERE
+	    	// By default, INFO and higher is logged to the console
+	    	// FINER will be reserved for received data packets only!!!
+	    	String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date());
+	    	fh = new FileHandler(timeStamp + "_groundstation.log", true); // Don't overwrite existing file 
+	        fh.setFormatter(new SimpleFormatter());
+	    	fh.setLevel(Level.ALL); // Everything will be logged to the file
+	        LOGGER.addHandler(fh);
+	        LOGGER.setLevel(Level.ALL);
+	        LOGGER.fine("Logging initialized succesfully.");
+	        LOGGER.fine("Starting Aero GroundStation");
+
+	    } catch (SecurityException e) {  
+	        e.printStackTrace();  
+	    } catch (IOException e) {  
+	        e.printStackTrace();  
+	    }  
+		
 		frame = new JFrame("Aero GUI");
 		//frame.setBounds(100, 100, 1200, 600);
 		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);  //set to full screen 
